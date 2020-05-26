fd3835e24e3f180df3517707b0bfb366419ce396
Jonathan Shearon <jonathan.shearon@gmail.com>
2020-05-26 17:47:40 -0500

    added pet dino feature

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ const updateHp = (dinoId, amount) => {
   } else {
     dino.health = dino.health + amount;
   }
+  return dino.health;
 }
 
 //process dino form
@@ -44,7 +45,8 @@ const dinoKennel = () => {
                     <div class="card-img-top">
                       <img class="dino-img ${dinos[i].walk}" src="${dinos[i].imageUrl}" alt="image of ${dinos[i].name}">
                       <img class="cell-bars" src="cell-bars.png">
-                      <h3 class="hp"><span class="badge badge-danger">HP ${dinos[i].health}</span></h3>
+                      <h3 class="hp"><span class="badge badge-danger" id="${dinos[i].id}-hp">HP ${dinos[i].health}</span></h3>
+                      <span class="hp-animate-span" id="animate-hp-${dinos[i].id}"></span>
                     </div>
                   <div class="card-body">
                     <h4 class="card-title">${dinos[i].name} <small class="text-muted"><em>${dinos[i].type}</em></small></h4>
@@ -58,14 +60,14 @@ const dinoKennel = () => {
     domString +=     `</ul>
                     </p>
                     <div class="d-flex justify-content-between">
-                      <button type="button" class="btn btn-success" data-toggle="tooltip" data-placement="top" title="Feed ${dinos[i].name}"><i class="fas fa-cookie-bite fa-2x"></i></button>
-                      <button type="button" class="btn btn-danger" data-toggle="tooltip" data-placement="top" title="Send ${dinos[i].name} to Hostpital"><i class="fas fa-hospital fa-2x"></i></button>
-                      <button type="button" class="btn btn-success" data-toggle="tooltip" data-placement="top" title="Pet ${dinos[i].name}"><i class="fas fa-hand-paper fa-2x"></i></button>
-                      <button type="button" class="btn btn-success" data-toggle="tooltip" data-placement="top" title="Send ${dinos[i].name} on an Advenure"><i class="fas fa-suitcase fa-2x"></i></button>
+                      <button type="button" class="btn btn-success dino-buttons" data-toggle="tooltip" data-placement="top" title="Feed ${dinos[i].name}"><i class="fas fa-cookie-bite fa-2x" data-dino-id="${dinos[i].id}" data-action="feed"></i></button>
+                      <button type="button" class="btn btn-danger dino-buttons" data-toggle="tooltip" data-placement="top" title="Send ${dinos[i].name} to Hostpital"><i class="fas fa-hospital fa-2x" data-dino-id="${dinos[i].id}" data-action="hospital"></i></button>
+                      <button type="button" class="btn btn-success dino-buttons" data-toggle="tooltip" data-placement="top" data-dino="${dinos[i].id}" title="Pet ${dinos[i].name}"><i class="fas fa-hand-paper fa-2x" data-dino-id="${dinos[i].id}" data-action="pet"></i></button>
+                      <button type="button" class="btn btn-success dino-buttons" data-toggle="tooltip" data-placement="top" title="Send ${dinos[i].name} on an Advenure"><i class="fas fa-suitcase fa-2x" data-dino-id="${dinos[i].id}" data-action="adventure"></i></button>
                     </div>    
                   </div>
                 </div>`
-    }
+    } 
   }
   printToDom("#dino-kennel", domString);
 }
@@ -85,8 +87,50 @@ const populateForm = () => {
   printToDom("#type", allTypes);
 }
 
+const animateHp = (dinoId, points) => {
+  domString = `<i class="fas fa-heart fa-2x hp-heart-on">+${points}</i>`;
+  printToDom('#animate-hp-' + dinoId, domString)
+}
+
+//const feedDino = (dinoId) => {
+//
+//}
+
+//const hospitalDino = (dinoId) => {
+//  
+//}
+
+const petDino = (dinoId) => {
+  const newHp = updateHp(dinoId, 1);
+  document.getElementById(dinoId + '-hp').innerHTML = 'HP ' + newHp;
+  animateHp(dinoId, 1);
+}
+
+//const adventureDino = (dinoId) => {
+//  
+//}
+
 const listeners = () => {
   document.getElementById("add-dino-form").addEventListener('submit', addDino);
+  document.querySelectorAll('.dino-buttons').forEach(item => {
+    item.addEventListener('click', e => {
+      const action = e.target.dataset.action;
+      const dinoId = e.target.dataset.dinoId;
+      switch (action) {
+        case 'feed':
+         // feedDino(dinoId);
+          break;
+        case 'hospital':
+         // hospitalDino(dinoId);
+          break;
+        case 'pet':
+          petDino(dinoId);
+          break;
+        case 'adventure':
+          //adventureDino(dinoId);
+      }
+    })
+  })
 }
 
 // show tootips
